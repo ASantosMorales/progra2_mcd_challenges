ad838039965daba01b477d443d9d559489e0fc21
ASantosMorales <alberto_santos@live.com.mx>
2025-03-25 21:07:52 -0600

    correction of upload_results.py

--- cancer_detection_challenge/upload_results.py
+++ cancer_detection_challenge/upload_results.py
@@ -1,8 +1,5 @@
 import os
-from mlops_pipeline import export_experiment_id
 
-experiment_id = export_experiment_id()
-
-os.system(f'git add mlruns/{experiment_id}*')
-os.system(f'git commit -m "adding new experiment to {experiment_id}"')
+os.system(f'git add mlruns/*')
+os.system(f'git commit -m "adding new experiment to run to experiments"')
 os.system('git push origin main:features/exp_results')
