47e6686c8b3c61f59ecdcde3043d7c3c0536a0f2
ASantosMorales <alberto_santos@live.com.mx>
2025-04-29 17:41:44 -0600

    first trial to change classes approach, it did not work well

--- comments_on_glassdoor_challenge/src/main.py
+++ comments_on_glassdoor_challenge/src/main.py
@@ -1,34 +1,35 @@
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
-from preprocessing import dataset, text_preprocessing
+from preprocessing import *
 from model_training import sentimentClassifier
 
 def main():
-	data = dataset()
-	df = data.load()
+	df = load_dataset()
+
+	# Select the text columns to be processed
 	columns = ['headline', 'pros', 'cons']
-	text = text_preprocessing()
 
-	df_cleaned, name_columns_cleaned = text.cleanning(df = df,
-													columns = columns,
-													lower_case = True,
-													remove_numbers = True,
-													remove_punctuation = True,
-													remove_extra_spaces = True)
+	df_cleaned, name_columns_cleaned = cleanning(df = df,
+												columns = columns,
+												lower_case = True,
+												remove_numbers = True,
+												remove_punctuation = True,
+												remove_extra_spaces = True)
 	
-	df_no_stopwords, name_columns_no_stopwords = text.stopwords_remotion(df = df_cleaned,
-																		columns = name_columns_cleaned) 
+	df_no_stopwords, name_columns_no_stopwords = stopwords_remotion(df = df_cleaned,
+																	columns = name_columns_cleaned) 
 	
-	df_lemmatized, name_columns_lemmatized = text.lemmatizing(df = df_no_stopwords,
-															columns = name_columns_no_stopwords)
+	df_lemmatized, name_columns_lemmatized = lemmatizing(df = df_no_stopwords,
+														columns = name_columns_no_stopwords)
 
-	df_text_merged, name_merged_column = text.text_columns_merging(df = df_lemmatized,
-																columns=name_columns_lemmatized)
+	df_text_merged, name_merged_column = text_columns_merging(df = df_lemmatized,
+															columns=name_columns_lemmatized)
 
 	columns_to_map = ['recommend']
-	df_lemmatized_mapped, name_columns_mapped = text.rank_mapping(df = df_text_merged,
-																columns = columns_to_map)
+	df_lemmatized_mapped, name_columns_mapped = rank_mapping(df = df_text_merged,
+															columns = columns_to_map)
 
+	"""
 	text_classifier = sentimentClassifier()
 	X = text_classifier.tfidf_vectorizing(df_lemmatized_mapped[name_merged_column])
 	y = df_lemmatized_mapped[name_columns_mapped[0]]
@@ -41,6 +42,7 @@ def main():
 
 	accurracy = accuracy_score(y_test, y_pred)
 	print(f'Accurracy score = {round(accurracy, 4)}')
+	"""
 
 if __name__ == '__main__':
 	main()
